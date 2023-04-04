0dcb439edf14abcb115141386ea096b7c691eb71
Nicolas Muller <muller.nic@gmail.com>
2023-04-04 20:09:31 -0300

    registro y login

--- pages/login/js/login.js
+++ pages/login/js/login.js
@@ -5,32 +5,36 @@ loginForm.addEventListener('submit',(event) => {
 
     const {email,password} = loginForm.elements;
     console.log(email.value,password.value)
+
+
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+
+    //2-Chequear datos ingresados
+        //a- Existe email
+        //b- Password deberian ser las mismas
+    
+    const user = users.find((usr) => {
+        if(usr.email === email.value){
+            return true;
+        }
+        return false;
+    })
+    
+    if(!user || user.password !== password.value) {
+        alert(`Los Datos ingresados no son correctos`);
+    }
+    
+    if(user.password === password.value){
+        localStorage.setItem('currentUser',JSON.stringify(user));
+        alert('Login correcto')
+    
+    }
+    //3- Guardar en el registro (LocalStorage) los datos de la persona logueada en ese momento.
+    
+    // Funcion LogOut
+    //1- Borrar el usuario del local storage
+
+
 })
 //
 
-const users = JSON.parse(localStorage.getItem('users')) || [];
-
-//2-Chequear datos ingresados
-    //a- Existe email
-    //b- Password deberian ser las mismas
-
-const user = users.find((usr) => {
-    if(usr.email === email.value){
-        return true;
-    }
-    return false;
-})
-
-if(!user || user.password !== password.value) {
-    alert(`Los Datos ingresados no son correctos`);
-}
-
-if(user.password === password.value){
-    localStorage.setItem('correntUser',JSON.stringify(user));
-    alert('Login correcto')
-
-}
-//3- Guardar en el registro (LocalStorage) los datos de la persona logueada en ese momento.
-
-// Funcion LogOut
-//1- Borrar el usuario del local storage
