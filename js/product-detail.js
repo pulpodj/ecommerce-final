9abc68b3d7b15f04ef56b1953620da54646e5421
Nicolas Muller <muller.nic@gmail.com>
2023-04-04 22:03:21 -0300

    Card en LocalStorage

--- js/product-detail.js
+++ js/product-detail.js
@@ -6,3 +6,9 @@ const paramsURL = new URLSearchParams(params);
 
 const paramsEntries = Object.fromEntries(paramsURL);
 
+const products = JSON.parse(localStorage.getItem('products'));
+
+const product = products[id];
+
+document.body.innerText = `<p>${JSON.stringify(product)}</p>`;
+
